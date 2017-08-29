b3248eea0eaa2033785124474a5718b26bfc732b
Armand Jordaan <armandj@neosystems.co.za>
2017-08-29 10:50:48 +0200

    Initial version

--- serialports/Program.cs
+++ serialports/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace serialports
 {
@@ -10,6 +11,27 @@ namespace serialports
     {
         static void Main(string[] args)
         {
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine(Environment.NewLine + "Serial port list:" + Environment.NewLine);
+
+            foreach(string s in ports)
+            {
+                Console.Write(s + "  ");
+
+                try
+                {
+                    SerialPort p = new SerialPort(s);
+                    p.Open();
+                    p.Close();
+                    Console.WriteLine();
+                }
+                catch
+                {
+                    Console.WriteLine("(In use)");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
